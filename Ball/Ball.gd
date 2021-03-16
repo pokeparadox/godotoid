0f7d294bfbcffcf04e89050ef21253965d838e05
Kevin Winfield-Pantoja <pokeparadox@gmail.com>
2021-03-16 22:21:29 +0000

    Ported Ball object from cpp to godot
    Created a Ball node based upon the cpp source

---
+++ Ball/Ball.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+# Declare member variables here. Examples:
+export var acceleration = 0.001
+var speed = 1.0
+var velocity = Vector2(speed,speed)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position += velocity * delta
+	speed += acceleration * delta
+	
+func _set_direction(newDirection):
+	velocity = newDirection
+	velocity = velocity.normalized()
+	velocity *= speed
