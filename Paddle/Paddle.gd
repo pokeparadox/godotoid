6e5de00b48ff458689ed70d9d72e53fb43eb41fa
Kevin Winfield-Pantoja <pokeparadox@gmail.com>
2021-03-17 21:01:02 +0000

    Added controls to paddle update project settings.
    -Added basic left/right control to Paddle.
    Performs the check in input but performs the movement using physics
    loop with delta values
    
    -Enabled multi threading mode and updated resolution.

--- Paddle/Paddle.gd
+++ Paddle/Paddle.gd
@@ -2,16 +2,26 @@ extends KinematicBody2D
 
 # Declare member variables here. Examples:
 export var moveRate = 3.0
+var movementDirection = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-# TODO check controls to move paddle
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	moveRight(delta)
-
+	if(movementDirection<0):
+		moveLeft(delta)
+	elif(movementDirection>0):
+		moveRight(delta)
+	
+func _input(event):
+	if(event.is_action("ui_left")):
+		movementDirection = -1
+	elif(event.is_action("ui_right")):
+		movementDirection = 1
+	else:
+		movementDirection = 0
 
 func moveLeft(delta):
 	position.x -= moveRate * delta
