e6ef41e56e11f37631e987db2fe19d16e7cb1f7c
Kevin Winfield-Pantoja <pokeparadox@gmail.com>
2021-03-20 19:37:47 +0000

    Added Win condition
    Added condition to play sound effect for winning.

--- GameState.gd
+++ GameState.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+var playCount = 6
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	setupBricks()
@@ -13,7 +13,11 @@ func setupBricks():
 			brick.position = Vector2((x*40)+halfWidth,y*16+halfHeight)
 			brick.add_to_group("Bricks")
 			add_child(brick)
-
+func _process(delta):
+	var bricks = get_tree().get_nodes_in_group("Bricks")
+	if(bricks.empty() && !$Win.playing && playCount > 0):
+		$Win.play()
+		playCount = playCount - 1
 
 func _on_Ball_BallHit():
 	$Audio.pitch_scale = Global.BallHeight
