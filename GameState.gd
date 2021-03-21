cc754423c982b398e566e096acdfbafd107cc610
Kevin Winfield-Pantoja <pokeparadox@gmail.com>
2021-03-21 00:31:17 +0000

    Added Scoring
    Added scoring system.
    -1 base point is added for each bounce.
    -Bounces act as score multiplier until death.
    -Bounce count reset to zero on death
    -Bricks give 50 basepoints
    -Clearing the level gives 1000 base points.
    -3 lives are provided.
    -clearing the level resets bricks.

--- GameState.gd
+++ GameState.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
+onready var bounceCount = 0
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	setLives(Global.StartLives)
+	setScore(0)
 	setupBricks()
 
 func setupBricks():
@@ -17,25 +22,32 @@ func setupBricks():
 func _on_Ball_BallHit():
 	$Audio.pitch_scale = Global.BallHeight
 	$Audio.play()
-
+	bounceCount = bounceCount + 1
+	addScore(Global.BouncePoint * bounceCount)
 
 func _on_Ball_Lose():
 	$Death.play()
-
-
+	bounceCount = 0
+	takeLife()
 func _on_Ball_BrickHit():
 	var bricks = get_tree().get_nodes_in_group("Bricks")
+	addScore(Global.BrickBreakPoints * bounceCount)
+	
 	if(bricks.empty() && !soundPlaying):
+		addScore(Global.BonusPoints * bounceCount)
+		Global.level = Global.level + 1
+		setLevel(Global.level)
 		$Ball.resetBall()
 		playWinSound()
 		setupBricks()
+		
 		
 var playCount = 6
 var soundPlaying = false
 func playWinSound():
 	soundPlaying = true
 	playCount = 6
-	$Win.play()			
+	$Win.play()
 
 func _on_Win_finished():
 	playCount = playCount - 1
@@ -44,3 +56,32 @@ func _on_Win_finished():
 	else:
 		soundPlaying = false
 		playCount = 6
+		
+func setLives(lives):
+	Global.lives = lives
+	$Lives.text = "Lives:" + String(lives)
+	
+func takeLife():
+	Global.lives = Global.lives - 1
+	setLives(Global.lives)
+	if(Global.lives < 0):	
+		if(Global.score > Global.hiScore):
+			setHiScore(Global.score)
+		setScore(0)
+		setLives(Global.StartLives)
+		setupBricks()
+	
+func setScore(points):
+	Global.score = points
+	$Score.text = "Score:" + String(points)
+
+func addScore(points):
+	setScore(Global.score + points)
+	
+func setLevel(lvl):
+	Global.level = lvl
+	$Level.text = "Lvl:" + String(lvl)
+	
+func setHiScore(hi):
+	Global.hiScore = hi
+	$HiScore.text = "Hi:" + String(hi)
--- Global.gd
+++ Global.gd
@@ -3,6 +3,15 @@ extends Node
 
 # Declare member variables here. Examples:
 var BallHeight = 1
+const BonusPoints = 1000
+const BrickBreakPoints = 50
+const BouncePoint = 1
+const StartLives = 3
+var score = 0
+var hiScore = 0
+var level = 1
+var lives = 3
+
 
 func setBallHeight(screenHeight):
 	BallHeight = 1 + ((OS.get_screen_size().y-screenHeight)/OS.get_screen_size().y)*3
