bc9f515de30328d201d8e16acebbd613bef3a120
Kevin Winfield-Pantoja <pokeparadox@gmail.com>
2021-03-27 22:37:58 +0000

    Fixed paddle responsiveness
    Corrected paddle responsiveness by using polled instead of events for
    input

--- Paddle/Paddle.gd
+++ Paddle/Paddle.gd
@@ -6,12 +6,12 @@ var movementDirection = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	var _result = move_and_collide(Vector2(movementDirection * (moveRate + Global.level * 0.001) , 0))
-	
-func _input(event):
-	if(event.is_action("ui_left") && !event.is_action_released("ui_left")):
+	if(Input.is_action_pressed("ui_left")):
 		movementDirection = -1
-	elif(event.is_action("ui_right") && !event.is_action_released("ui_right")):
+	elif(Input.is_action_pressed("ui_right")):
 		movementDirection = 1
 	else:
 		movementDirection = 0
+		
+	var _result = move_and_collide(Vector2(movementDirection * (moveRate + Global.level * 0.001) , 0))
+	
