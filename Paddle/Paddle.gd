32ccb10a484b945495c483df655041cf15bc094e
Kevin Winfield-Pantoja <pokeparadox@gmail.com>
2021-06-20 13:14:08 +0100

    Added basic touch controls
    Pressing the Left and Right of the game screen will now move the paddle.

--- Paddle/Paddle.gd
+++ Paddle/Paddle.gd
@@ -3,6 +3,9 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 export var moveRate = 1.5
 var movementDirection = 0
+var leftPressed = false
+var rightPressed = false
+
 onready var startHeight = position.y
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,6 +19,28 @@ func _process(_delta):
 		movementDirection = 1
 	else:
 		movementDirection = 0
+	
+	if(movementDirection == 0 ):
+		if(leftPressed && !rightPressed):
+			movementDirection = -1
+		if(rightPressed && !leftPressed):
+			movementDirection = 1
 		
 	var _result = move_and_collide(Vector2(movementDirection * (moveRate + Global.level * 0.1) , 0))
 	
+
+
+func _on_TouchLeft_pressed():
+	leftPressed = true
+
+
+func _on_TouchRight_pressed():
+	rightPressed = true
+
+
+func _on_TouchRight_released():
+	rightPressed = false
+
+
+func _on_TouchLeft_released():
+	leftPressed = false
